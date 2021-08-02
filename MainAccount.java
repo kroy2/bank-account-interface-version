62b0295914d76a5de385ebc8648162f5aeb639e2
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-02 17:38:41 -0600

    Update MainAccount.java

--- MainAccount.java
+++ MainAccount.java
@@ -1,1 +1,31 @@
+package database;
+
+public abstract class MainAccount {
+
+	private UserInfo userInfo;
+	
+	// creation of a constructor linking userInfo class
+	public MainAccount(UserInfo userInfo) {
+		this.userInfo = userInfo;
+	}
+	
+	// getter method
+	public UserInfo getUserInfo() {
+		return userInfo;
+	}
+	
+	public void depositMoney(int amountOfMoney) {
+		
+	}
+	
+	public boolean withdrawMoney(int amountOfMoney) { // need to implement
+		if(withdrawable(amountOfMoney)) {
+	}
+		return false; 
+}
+
+	private boolean withdrawable(int amountOfMoney) {
+		return userInfo.getAccountBalance()>=amountOfMoney;
+	}
+}
 
