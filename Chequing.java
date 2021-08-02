8fd9606ea81317f505286fa705f7f2784b1a0cd1
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-02 17:33:11 -0600

    Update Chequing.java

--- Chequing.java
+++ Chequing.java
@@ -1,1 +1,9 @@
+package database;
+
+public class Chequing extends MainAccount{
+	// creation of constructors in order to have the user information here
+	public Chequing(UserInfo userInfo) {
+		super(userInfo);
+	}
+}
 
