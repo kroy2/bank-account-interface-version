c27264ea9f66f841b873650fd48dccb787486077
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-02 17:32:45 -0600

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,1 +1,9 @@
+package database;
+
+public class Savings extends MainAccount {
+	// creation of constructors in order to have the user information here
+	public Savings(UserInfo userInfo) {
+		super(userInfo);
+	}
+}
 
