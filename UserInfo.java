1f56b7b8c02592b1a6881db001ddc16b1828bb9e
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-02 17:31:51 -0600

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,1 +1,33 @@
+package database;
+
+public class UserInfo {
+
+	// immutable structures so they cannot be altered upon creation
+	private final String name;
+	private final String username;
+	private final int password;	
+	private final int accountBalance;
+	
+	// creation of a constructor
+	public UserInfo(String name, String username, int password, int accountBalance) {
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.accountBalance = accountBalance;
+	}
+	
+	// getter methods for basic functionalities of previously assigned database variables
+	public String getName() {
+		return name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public int accountBalance() {
+		return accountBalance;
+	}
+}
 
