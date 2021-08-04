e96d45d419e43038463d53b724b865163021edad
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-03 18:29:30 -0600

    Delete Savings.java

--- Savings.java
+++
@@ -1,8 +0,0 @@
-package database;
-public class Savings extends MainAccount {
-	// creation of constructors in order to have the user information here
-	public Savings(UserInfo userInfo) {
-		super(userInfo);
-	}
-}
-
