b31dcf6917e5421d40b26db3df349373be4dd4c2
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-08-03 21:15:09 -0600

    Added all functionalites, need to edit methods

--- Controllers/UserInfoViewController.java
+++ Controllers/UserInfoViewController.java
@@ -1,5 +1,42 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UserInfoViewController {
+
+    @FXML
+    private TextField UsernameInput;
+
+    @FXML
+    private Label ResetTextOutput;
+
+    @FXML
+    private Button ResetButton;
+
+    @FXML
+    private TextField PasswordInput;
+
+    @FXML
+    private Button SubmitButton;
+
+    @FXML
+    void ResetButtonClicked(ActionEvent event) {
+    	UsernameInput.clear();
+    	PasswordInput.clear();
+    	ResetTextOutput.setText("Your information has been Reset, please enter information.");
+    }
+
+    @FXML
+    void SubmitButtonClicked(ActionEvent event) {
+
+    }
+
+	public void linkWithApplication(UserInfoViewTester userInfoViewTester) {
+		
+	}
 
 }
