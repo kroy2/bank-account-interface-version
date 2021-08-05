7cfc9c402f7585e8ed240c9f287db978c292a60a
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-08-04 18:26:59 -0600

    Adds additional functionalities and better comments

--- Controllers/AccountsViewController.java
+++ Controllers/AccountsViewController.java
@@ -1,10 +1,19 @@
 package Controllers;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AccountsViewController {
 
@@ -12,7 +21,25 @@ public class AccountsViewController {
 	private TextField WithdrawMoneyInputChequing;
 
 	@FXML
+	private Label UsernameLabel2;
+
+	@FXML
 	private Label SavingsAccountBalanceLabel1;
+
+	@FXML
+	private Button LogOutButton;
+
+	@FXML
+	private Button DepositButtonChequing;
+
+	@FXML
+	private Label UsernameLabel;
+
+	@FXML
+	private Label ChequingAccountBalanceLabel;
+
+	@FXML
+	private Button LogOutButton2;
 
 	@FXML
 	private TextField DepositMoneyInputSavings;
@@ -30,25 +57,38 @@ public class AccountsViewController {
 	private Button WithdrawButtonChequing;
 
 	@FXML
-	private Label SavingsAccountBalanceLabel;
-
-	@FXML
-	private Button DepositButtonChequing;
-
-	@FXML
-	private Label ChequingAccountBalanceLabel;
+	private Button LogOutButton3;
 
 	@FXML
 	private TextField DepositMoneyInputChequing;
 
 	@FXML
 	void DepositButtonClickedChequing(ActionEvent event) {
+		// Deposit method invoked
 
+		//
 	}
 
 	@FXML
 	void WithdrawButtonClickedChequing(ActionEvent event) {
+		// Withdraw method invoked
+	}
 
+	@FXML
+	void LogOut(ActionEvent event) throws FileNotFoundException, IOException {
+		FXMLLoader loader = new FXMLLoader();
+		// Access UserInfoView fxml file to set new scene
+		Parent UserInfoViewParent = (Parent) loader.load(new FileInputStream("src/Views/UserInfoView.fxml"));
+
+		// Sets scene
+		Scene UserInfoViewScene = new Scene(UserInfoViewParent);
+
+		// This line gets the stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		// Sets scene and shows upon button press
+		window.setScene(UserInfoViewScene);
+		window.show();
 	}
 
 	@FXML
@@ -66,7 +106,7 @@ public class AccountsViewController {
 	 * 
 	 */
 	public void linkWithApplication(AccountsViewTester accountsViewTester) {
-		
+
 	}
 
 }
