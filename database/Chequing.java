742ff2068fd0815f649150dc1d857724d029c9fb
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-04 18:07:39 -0600

    Delete Chequing.java

--- database/Chequing.java
+++
@@ -1,8 +0,0 @@
-package database;
-
-public class Chequing extends MainAccount{
-	// creation of constructors in order to have the user information here
-	public Chequing(UserInfo userInfo) {
-		super(userInfo);
-	}
-}
