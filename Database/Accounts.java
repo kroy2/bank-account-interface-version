985505c795539d033b8cfc597d4079b122f922d6
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-04 18:09:38 -0600

    Update Accounts.java

--- Database/Accounts.java
+++ Database/Accounts.java
@@ -1,1 +1,24 @@
+package Database;
+
+public abstract class Accounts {
+	private AccountBalance accountBalance;
+
+	public Accounts(AccountBalance accountBalance) {
+		this.accountBalance=accountBalance;
+	}
+	public AccountBalance getAccountBalance() {
+		return accountBalance;
+	}
+	
+	private void newBalance(int updatedBalance) {
+		accountBalance = new AccountBalance(updatedBalance);
+	}
+	
+	public void depositMoney(int amount) {
+		newBalance(getAccountBalance()+amount);
+	}
+	public boolean withdrawMoney(int amount) {
+		if()
+	}
+}
 
