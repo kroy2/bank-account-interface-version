f6bf9b6921cc756ac1427ebb6c6d892914b2e16f
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-05 22:24:14 -0600

    Update LowRiskViewController.java

--- Controllers/LowRiskViewController.java
+++ Controllers/LowRiskViewController.java
@@ -1,5 +1,64 @@
 package Controllers;
 
-public class LowRiskViewController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HighRiskViewController {
+
+    @FXML
+    private Button GoBackButton;
+
+    @FXML
+    private TextField HighRiskDepositInput;
+
+    @FXML
+    private Label LowRiskTFSABalance;
+
+    @FXML
+    private Button LowRiskDepositButton;
+
+    @FXML
+    private Button LowestInvestmentOptionButton;
+
+    @FXML
+    private Button HighInvestmentOptionButton;
+
+    @FXML
+    private Label InvestmentOptionOutput;
+
+    @FXML
+    private Button MiddleInvestmentOptionButton;
+
+    @FXML
+    void HighRiskDepositButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LowRiskDepositButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LowInvestmentButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MiddleInvestmentButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HighInvestmentButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoBackButtonClicked(ActionEvent event) {
+
+    }
 
 }
