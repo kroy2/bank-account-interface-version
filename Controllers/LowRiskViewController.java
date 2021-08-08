4cff407e80cba7f3d186c7ef4ebb31cf3d0d4026
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-08 17:23:12 -0600

    Update LowRiskViewController.java

--- Controllers/LowRiskViewController.java
+++ Controllers/LowRiskViewController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class HighRiskViewController {
+public class LowRiskViewController {
 
     @FXML
     private Button GoBackButton;
