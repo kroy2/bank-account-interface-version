e56f7d851e5dac900441101673f8914220760f57
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-08-08 18:25:33 -0600

    Update LowRiskViewTester.java

--- Controllers/LowRiskViewTester.java
+++ Controllers/LowRiskViewTester.java
@@ -1,5 +1,41 @@
 package Controllers;
 
-public class LowRiskViewTester {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LowRiskViewTester extends Application {
+
+	Stage primaryStage;
+	
+	private void LowRiskView() {
+		FXMLLoader loader = new FXMLLoader();
+		Scene scene;
+		try {
+			Parent LowRiskView = (Parent) loader.load(new FileInputStream("src/Views/LowRiskView.fxml"));
+			LowRiskViewController LowRiskViewController = loader.getController();
+			LowRiskViewController.LinkWithApplication(this);
+			scene = new Scene(LowRiskView, 800, 600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		LowRiskView();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
